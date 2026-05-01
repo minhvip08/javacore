075c68ee2a0f97da6d46ea46f0a033e8c0412bff
minhvip08 <minhvip08@gmail.com>
2026-05-01 11:21:21 +0700

    feat: refactor to Simi learning docs

---
+++ java/section_10/src/com/simi/marvel/app/ConstructorDemo.java
@@ -0,0 +1,12 @@
+package com.simi.marvel.app;
+
+import com.simi.marvel.vehicle.Car;
+
+public class ConstructorDemo {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.madeFor);
+    }
+
+}
